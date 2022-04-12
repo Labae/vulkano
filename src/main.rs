b86771179ef780176768c14c5d4759bb26590122
Sungsik Bae <46157657+Labae@users.noreply.github.com>
2022-04-12 13:27:42 +0900

    add simple rust project

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn double(input: i32) -> i32 {
+    input * 2
+}
+
+fn triple(input: i32) -> i32 {
+    input * 3
+}
+
+fn main() {
+    let x = 9;
+    let x = double(x);
+    let x = triple(x);
+
+    println!("{}", x);
+}
