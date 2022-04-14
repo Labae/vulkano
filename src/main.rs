25f12862d2c58c13bedd2fd691d7556419f8aab4
Sungsik Bae <46157657+Labae@users.noreply.github.com>
2022-04-14 15:50:22 +0900

    add window init

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,49 @@
-#![allow(
-    dead_code,
-    unused_variables,
-    clippy::too_many_arguments,
-    clippy::unnecessary_wraps
-)]
-
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{EventLoop, ControlFlow},
+    window::{WindowBuilder, Window},
+    dpi::LogicalSize};
 use anyhow::Result;
-use winit::dpi::LogicalSize;
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::{Window, WindowBuilder};
 
-fn main() -> Result<()> {
+fn main() {
+     // to use a visually nice colored log.
     pretty_env_logger::init();
 
-    // Window
+    let width: i32 = 1024;
+    let height: i32 = 768;
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("Vulkan Tutorial (Rust)")
-        .with_inner_size(LogicalSize::new(1024, 768))
-        .build(&event_loop)?;
+            .with_title("Vulkan Tutorial (Rust)")
+            .with_inner_size(LogicalSize::new(width, height))
+            .build(&event_loop).unwrap();
 
-    // App
-
-    let mut app = unsafe { App::create(&window)? };
+    let mut app = App::create(&window).unwrap();
     let mut destroying = false;
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
-            // Render a frame if our Vulkan app is not being destroyed.
-            Event::MainEventsCleared if !destroying =>
-                unsafe { app.render(&window) }.unwrap(),
-            // Destroy our Vulkan app.
+            Event::MainEventsCleared if !destroying => app.render(&window).unwrap(),
             Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
                 destroying = true;
                 *control_flow = ControlFlow::Exit;
-                unsafe { app.destroy(); }
+                app.destroy();
             }
             _ => {}
         }
     });
 }
 
-/// Our Vulkan app.
-#[derive(Clone, Debug)]
 struct App {}
 
 impl App {
-    /// Creates our Vulkan app.
-    unsafe fn create(window: &Window) -> Result<Self> {
+    fn create(window: &Window) -> Result<Self> {
         Ok(Self {})
     }
 
-    /// Renders a frame for our Vulkan app.
-    unsafe fn render(&mut self, window: &Window) -> Result<()> {
+    fn render(&mut self, window: &Window) -> Result<()> {
         Ok(())
     }
 
-    /// Destroys our Vulkan app.
-    unsafe fn destroy(&mut self) {}
+    fn destroy(&mut self) {}
 }
-
-/// The Vulkan handles and associated properties used by our Vulkan app.
-#[derive(Clone, Debug, Default)]
-struct AppData {}
